c158d9d7047ef3b3d41010c2cbb9ad1423d39e39
ShwetaChauhan18 <shwetachauhan655@gmail.com>
2019-06-23 11:41:56 +0530

    fix:   - fragment crash issue fix

--- app/src/main/java/com/egoriku/testapplication/activity/MainActivity.kt
+++ app/src/main/java/com/egoriku/testapplication/activity/MainActivity.kt
@@ -2,9 +2,14 @@ package com.egoriku.testapplication.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import com.egoriku.testapplication.R
 import com.egoriku.testapplication.fragment.CrashFragment
+import com.egoriku.testapplication.viewmodel.TestViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.androidx.scope.currentScope
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/egoriku/testapplication/fragment/CrashFragment.kt
+++ app/src/main/java/com/egoriku/testapplication/fragment/CrashFragment.kt
@@ -7,14 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.egoriku.testapplication.R
+import com.egoriku.testapplication.service.IService
 import com.egoriku.testapplication.viewmodel.TestViewModel
 import kotlinx.android.synthetic.main.fragment_crash.*
+import org.koin.android.ext.android.getKoin
 import org.koin.androidx.scope.currentScope
+import org.koin.androidx.viewmodel.ext.android.getViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.qualifier.named
 
 class CrashFragment : Fragment() {
 
-    private val model: TestViewModel by currentScope.viewModel(this)
+    private val viewModelScope = getKoin().getOrCreateScope("IService",named("CrashFragment"))
+    private val model: TestViewModel = viewModelScope.get()
+    //private val model: TestViewModel by currentScope.viewModel(this)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_crash, container, false)
@@ -26,5 +32,10 @@ class CrashFragment : Fragment() {
         model.liveData.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModelScope.close()
     }
 }
